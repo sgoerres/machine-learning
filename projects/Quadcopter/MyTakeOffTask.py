085811bd0312dbc2b3c494abc6e53f96f0009037
goerr001 <goerr001@DE150653>
2018-11-09 15:04:31 +0100

    some trying around with rewards

--- projects/Quadcopter/MyTakeOffTask.py
+++ projects/Quadcopter/MyTakeOffTask.py
@@ -35,7 +35,9 @@ class MyTakeOffTask():
         # position close to target pos: =.5
         # phi, theta, psi close to 0 (almost fully stable quadcopter): = .2
 
-        # Step 1: Find out how far away I am from the target (only x,y,z relevant)
+        # Step 1: get all info about total move to make
+
+        distance_length_z_max = abs(self.init_pose[2] - self.target_pos[2])
         distance_vectormax = self.init_pose[:3] - self.target_pos
         distance_vector_squared_max = np.square(distance_vectormax)
         distance_length_max = math.sqrt(np.sum(distance_vector_squared_max))
@@ -45,34 +47,30 @@ class MyTakeOffTask():
         distance_vector_squared = np.square(distance_vector)
         
         distance_length = math.sqrt(np.sum(distance_vector_squared))
+        distance_length_z = (self.target_pos[2] - self.sim.pose[2])
+        # Step 2: punish being to far away
+        reward = - ((distance_length_z)/distance_length_max) # normalize against total path
 
-        reward = 1 - ((distance_length)/distance_length_max) # normalize against total path
+        # Step 3: reward rising from the ground
+        distance_length2 = (self.sim.pose[2] - self.init_pose[2])
 
-        # Step 1a: put an incentive out for starting vertically and not wobbling much
-        reward -= self.sim.pose[5]  # make psi as close to zero as possible => thus start vertically
-        reward -= self.sim.pose[4]  # make theta as close to zero as possible => thus start vertically
-        reward -= self.sim.pose[3]  # make phi as close to zero as possible => thus start vertically
+        if distance_length_z >= -0.2:
+            reward += distance_length2/distance_length_z_max
 
-        ## Step 2: reward rising from the ground
-        #distance_vector2 = self.init_pose[:3] - self.sim.pose[:3]
-#
-        #distance_vector_squared2 = np.square(distance_vector2)
-#
-        #distance_l#ength2 = math.sqrt(np.sum(distance_vector_squared2))
-        #reward += distance_length2
+        #  ## Step 2: Punish if going too fast in the end
+        #  if distance_length < 0.2:
+        #     if abs(self.sim.v[0]) >= 0.2:
+        #        reward -= 1
+        #    if abs(self.sim.v[1]) >= 0.2:
+        #        reward -= 1
+        #    if abs(self.sim.v[2]) >= 0.2:
+        #        reward -= 1
 
-        # Step 2: Punish if going too fast
-        if distance_length < 0.2:
-            if abs(self.sim.v[0]) >= 0.2:
-                reward -= 0.1
-            if abs(self.sim.v[1]) >= 0.2:
-                reward -= 0.1
-            if abs(self.sim.v[2]) >= 0.2:
-                reward -= 0.1
-            
+        if reward < -25:  # this is taking a turn for the worse
+            reward = -100
         # Step 3: Punish if finished early (taken from https://study-hall.udacity.com/sg-465545-9999/rooms/community:nd009t:465545-cohort-9999-project-1189/community:thread-10335140521-256557?contextType=room)
         if self.sim.done and self.sim.runtime > self.sim.time:
-            reward = -1
+            reward = -100
 
         return reward
 
